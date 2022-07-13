612a6be53e2c03f434918a403356e00a7627843d
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-07-12 22:41:10 -0300

    feat: Initial commit

---
+++ src/main/kotlin/br/com/ifuture/IfutureApplication.kt
@@ -0,0 +1,11 @@
+package br.com.ifuture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IfutureApplication
+
+fun main(args: Array<String>) {
+	runApplication<IfutureApplication>(*args)
+}
