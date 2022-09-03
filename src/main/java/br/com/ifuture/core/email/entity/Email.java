364dcb58c213f18917e315c71322d12323c93952
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-09-03 10:18:17 -0300

    feat: Email entity

---
+++ src/main/java/br/com/ifuture/core/email/entity/Email.java
@@ -0,0 +1,55 @@
+package br.com.ifuture.core.email.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Builder
+public class Email {
+    
+    @Id
+    @Column
+    private String id;
+    
+    @Column
+    private String recipientName;
+    
+    @Column
+    private String recipientEmail;
+    
+    @Lob
+    @Column
+    private String text;
+
+    @Column
+    private LocalDateTime createdAt;
+    
+    @Column
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        if(createdAt == null) createdAt = LocalDateTime.now();
+        if(updatedAt == null) updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+
+}
