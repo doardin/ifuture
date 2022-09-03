f4a8a6d1256d74693d1c542c82630f7d339395e2
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-09-03 09:30:44 -0300

    Initial Commit

---
+++ src/main/java/br/com/ifuture/IfutureApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfutureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifuture/IfutureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
