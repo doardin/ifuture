334d4aef48d533b56de22236e89dc59ded1bf0bb
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-09-04 14:11:10 -0300

    feat: Email application service

--- src/main/java/br/com/ifuture/application/email/service/EmailApplicationService.java
+++ src/main/java/br/com/ifuture/application/email/service/EmailApplicationService.java
@@ -2,7 +2,23 @@ package br.com.ifuture.application.email.service;
 
 import org.springframework.stereotype.Service;
 
+import br.com.ifuture.application.email.dto.PostEmailDto;
+import br.com.ifuture.core.email.entity.Email;
+import br.com.ifuture.core.email.repository.EmailRepository;
+
 @Service
 public class EmailApplicationService {
     
+    private EmailRepository repository;
+
+    public void persistEmail(PostEmailDto dto){
+        Email email = Email.builder()
+            .recipientName(dto.getName())
+            .recipientEmail(dto.getEmail())
+            .text(dto.getText())
+        .build();
+        
+        this.repository.save(email);
+    }
+
 }
