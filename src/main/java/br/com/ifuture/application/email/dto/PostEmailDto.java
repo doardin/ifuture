ef56629f9e33cc6854a698884cf7388aa0e75a14
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-09-04 14:10:54 -0300

    feat: Email Dto

---
+++ src/main/java/br/com/ifuture/application/email/dto/PostEmailDto.java
@@ -0,0 +1,33 @@
+package br.com.ifuture.application.email.dto;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import br.com.ifuture.constants.ValidationMessages;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostEmailDto {
+    
+    @NotNull(message = ValidationMessages.REQUIRED)
+    @NotBlank(message = ValidationMessages.REQUIRED)
+    private String name;
+    
+    @NotNull(message = ValidationMessages.REQUIRED)
+    @NotBlank(message = ValidationMessages.REQUIRED)
+    @Email(message = ValidationMessages.VALID_EMAIL)
+    private String email;
+    
+    @NotNull(message = ValidationMessages.REQUIRED)
+    @NotBlank(message = ValidationMessages.REQUIRED)
+    private String text;
+    
+    @Future(message = ValidationMessages.VALID_DATE)
+    private LocalDate sendAt;
+}
---
+++ src/main/java/br/com/ifuture/constants/ValidationMessages.java
@@ -0,0 +1,7 @@
+package br.com.ifuture.constants;
+
+public class ValidationMessages {
+    public static final String REQUIRED = "Este campo é obrigatório.";
+    public static final String VALID_DATE = "Insira uma data válida.";
+    public static final String VALID_EMAIL = "Insira um email válido.";
+}
