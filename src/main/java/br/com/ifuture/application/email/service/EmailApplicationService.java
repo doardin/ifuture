fb1171c992330720e456ee0e1faa6462539da7f2
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-09-04 14:25:04 -0300

    fix: NullPointer and set a id for Email

--- src/main/java/br/com/ifuture/application/email/service/EmailApplicationService.java
+++ src/main/java/br/com/ifuture/application/email/service/EmailApplicationService.java
@@ -1,18 +1,23 @@
 package br.com.ifuture.application.email.service;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import br.com.ifuture.application.email.dto.PostEmailDto;
 import br.com.ifuture.core.email.entity.Email;
 import br.com.ifuture.core.email.repository.EmailRepository;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class EmailApplicationService {
     
-    private EmailRepository repository;
+    private final EmailRepository repository;
 
     public void persistEmail(PostEmailDto dto){
         Email email = Email.builder()
+            .id(UUID.randomUUID().toString())
             .recipientName(dto.getName())
             .recipientEmail(dto.getEmail())
             .text(dto.getText())
